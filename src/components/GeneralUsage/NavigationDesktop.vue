b17f6fa2728a140cbca5199642ddfbfb020296ea
Mariusz <mariusz.zalewski@senetic.com>
2023-12-27 19:01:54 +0100

    testing search bar

--- src/components/GeneralUsage/NavigationDesktop.vue
+++ src/components/GeneralUsage/NavigationDesktop.vue
@@ -4,6 +4,16 @@
             <h2 class="fw-semibold">Timetable</h2>
         </div>
         <div class="container bg-white rounded-1">
+
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2 with-magnifying-glass" type="search" placeholder="Search"
+                            aria-label="Search">
+                    </form>
+                </div>
+            </nav>
+
             <nav class="navbar navbar-expand bg-body-tertiary p-0">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -42,5 +52,10 @@ export default {
 <style lang="scss" scoped>
 .active {
     border-bottom: 3px solid var(--bs-primary);
+}
+
+.with-magnifying-glass {
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>') no-repeat right 10px center;
+    padding-right: 30px;
 }
 </style>
